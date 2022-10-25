ede7a27c9e0abcbf3a013ee85d4444c263a73fc3
Merey Meyerbekova <200107051@stu.sdu.edu.kz>
2022-10-26 01:47:28 +0600

    constraint layout

--- app/src/main/java/com/example/finalproject2/view/MainActivity.kt
+++ app/src/main/java/com/example/finalproject2/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.finalproject2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
