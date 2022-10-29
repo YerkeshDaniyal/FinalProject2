4ca55bd79a41248d5548c027971c290d391db0d2
Yerkesh <200107036@stu.sdu.edu.kz>
2022-10-30 01:06:41 +0600

    override fun onCreate

--- app/src/main/java/com/example/finalproject2/view/MainActivity.kt
+++ app/src/main/java/com/example/finalproject2/view/MainActivity.kt
@@ -12,10 +12,22 @@ private lateinit var binding: ActivityMainBinding
 private lateinit var homeFragment: HomeFragment
 private lateinit var searchFragment: SearchFragment
 
+private var currentFragment: Fragment? = null
 
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-class MainActivity : AppCompatActivity() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        homeFragment = HomeFragment()
+        searchFragment = SearchFragment()
+
+        binding.mainNavBottom.setOnNavigationItemSelectedListener(this)
+        binding.mainNavBottom.selectedItemId = R.id.menu_home
+
+    }
 
 
 
