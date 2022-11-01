a6ff84dde1a00440dd5f0880f41f0267061163d6
Merey Meyerbekova <200107051@stu.sdu.edu.kz>
2022-11-01 21:52:28 +0600

    function - fetching city

--- app/src/main/java/com/example/finalproject2/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/finalproject2/viewmodel/SearchViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.finalproject2.viewmodel
 
-class SearchViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.finalproject2.model.IViewProgress
+import com.example.finalproject2.model.MainRepository
+import com.example.finalproject2.model.WeatherApiResult
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SearchViewModel(private val view: IViewProgress, private val repository: MainRepository): ViewModel() {
+
+    val searchCity = MutableLiveData<WeatherApiResult>()
+    val errorMessage = MutableLiveData<String>()
+
+    fun fetchCity(city: String) {
+        view.showProgress(true)
+        val request = repository.fetchCity(city)
+
+
+    }
 }
