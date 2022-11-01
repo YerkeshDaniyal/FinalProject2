4aae436edeecd7b7ab948353d7e186ac3d5c42dd
Merey Meyerbekova <200107051@stu.sdu.edu.kz>
2022-11-01 21:47:54 +0600

    Adding Search view model to viewmodel

--- app/src/main/java/com/example/finalproject2/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/finalproject2/viewmodel/SearchViewModel.kt
@@ -1,37 +1,4 @@
 package com.example.finalproject2.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.example.finalproject2.model.IViewProgress
-import com.example.finalproject2.model.MainRepository
-import com.example.finalproject2.model.WeatherApiResult
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
-class SearchViewModel(private val view: IViewProgress, private val repository: MainRepository): ViewModel() {
-
-    val searchCity = MutableLiveData<WeatherApiResult>()
-    val errorMessage = MutableLiveData<String>()
-
-    fun fetchCity(city: String) {
-        view.showProgress(true)
-        val request = repository.fetchCity(city)
-
-        request.enqueue(object : Callback<WeatherApiResult>{
-            override fun onResponse(
-                call: Call<WeatherApiResult>,
-                response: Response<WeatherApiResult>
-            ) {
-                view.showProgress(false)
-                if (response.isSuccessful) searchCity.postValue(response.body())
-                else errorMessage.postValue("City not found")
-            }
-
-            override fun onFailure(call: Call<WeatherApiResult>, t: Throwable) {
-                errorMessage.postValue("Server error")
-            }
-
-        })
-    }
+class SearchViewModel {
 }
