093ca6e5b4546372b592e3a63ea0abb09767dea7
Merey Meyerbekova <200107051@stu.sdu.edu.kz>
2022-11-01 21:38:01 +0600

    7-8 homework

--- app/src/main/java/com/example/finalproject2/view/MainActivity.kt
+++ app/src/main/java/com/example/finalproject2/view/MainActivity.kt
@@ -2,11 +2,73 @@ package com.example.finalproject2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.R
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.example.finalproject2.R
+import com.example.finalproject2.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityMainBinding
+private lateinit var homeFragment: HomeFragment
+private lateinit var searchFragment: SearchFragment
+
+private var currentFragment: Fragment? = null
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        homeFragment = HomeFragment()
+        searchFragment = SearchFragment()
+
+        binding.mainNavBottom.setOnNavigationItemSelectedListener(this)
+        binding.mainNavBottom.selectedItemId = R.id.menu_home
+
+
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_home -> {
+                if (currentFragment == homeFragment) return false
+                currentFragment = homeFragment
+            }
+            R.id.menu_search -> {
+                if (currentFragment == searchFragment) return false
+                currentFragment = searchFragment
+            }
+        }
+
+        currentFragment?.let {
+            replaceFragment(R.id.fragment, it)
+        }
+
+        return true
+    }
+
+
+    private fun replaceFragment(id: Int, fragment: Fragment) {
+
+        if (supportFragmentManager.findFragmentById(id) == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(id, fragment)
+                commit()
+            }
+        } else {
+            supportFragmentManager.beginTransaction().apply {
+                replace(id, fragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+    }
+
+
 }
+
---
+++ app/src/main/java/com/example/finalproject2/viewmodel/MainViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.finalproject2.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.finalproject2.model.IViewProgress
+import com.example.finalproject2.model.MainRepository
+
+class MainViewModelFactory(private val view: IViewProgress, private val repository: MainRepository): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(this.view, this.repository) as T
+        }
+        throw IllegalArgumentException("ViewModel Not Found")
+    }
+
+}
