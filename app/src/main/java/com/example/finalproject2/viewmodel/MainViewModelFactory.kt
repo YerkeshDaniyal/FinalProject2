e0622e98338088010227ee69d43be948274b3323
Yerkesh <200107036@stu.sdu.edu.kz>
2022-11-02 07:16:32 +0600

    import lifecycleviewmodelprovider

--- app/src/main/java/com/example/finalproject2/viewmodel/MainViewModelFactory.kt
+++ app/src/main/java/com/example/finalproject2/viewmodel/MainViewModelFactory.kt
@@ -5,13 +5,3 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.finalproject2.model.IViewProgress
 import com.example.finalproject2.model.MainRepository
 
-class MainViewModelFactory(private val view: IViewProgress, private val repository: MainRepository): ViewModelProvider.Factory {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            return MainViewModel(this.view, this.repository) as T
-        }
-        throw IllegalArgumentException("ViewModel Not Found")
-    }
-
-}
