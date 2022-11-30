2108f16a75f77dd68b6809dfb6f09efdb2e7639b
YerkeshDaniyal <116493126+YerkeshDaniyal@users.noreply.github.com>
2022-11-30 08:44:16 +0600

    data class PhotoReferenceResponce

--- app/src/main/java/com/example/finalproject2/model/PhotoReferenceResponse.kt
+++ app/src/main/java/com/example/finalproject2/model/PhotoReferenceResponse.kt
@@ -3,5 +3,18 @@ package com.example.finalproject2.model
 import com.google.gson.annotations.SerializedName
 import retrofit2.http.Field
 
+data class PhotoReferenceResponse(
+    val candidates: Array<Place>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as PhotoReferenceResponse
 
+        if (!candidates.contentEquals(other.candidates)) return false
+
+        return true
+    }
+
+}
