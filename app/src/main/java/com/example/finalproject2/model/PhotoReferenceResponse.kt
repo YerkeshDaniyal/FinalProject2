958587390956bd1de56d489436e98fd5836afe8b
YerkeshDaniyal <116493126+YerkeshDaniyal@users.noreply.github.com>
2022-11-30 09:05:06 +0600

    override fun equals and hashcode

--- app/src/main/java/com/example/finalproject2/model/PhotoReferenceResponse.kt
+++ app/src/main/java/com/example/finalproject2/model/PhotoReferenceResponse.kt
@@ -24,5 +24,18 @@ data class PhotoReferenceResponse(
 data class Place(
     val photos: Array<PlacePhoto>
 ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as Place
+
+        if (!photos.contentEquals(other.photos)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return photos.contentHashCode()
+    }
 }
